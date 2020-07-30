f6890753964771ad3b06acd837a37edaa002544e
chupan1218 <1253126710@qq.com>
2020-07-30 19:22:38 +0800

    first commit

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+
+
+#' Hello world
+#' This is a function to say hello to the world.
+#'
+#' @return hello returns a string named hello world.
+#' @export
+#'
+#' @examples
+#' library(learning)
+#' hello()
+hello <- function(){
+  print("hello world")
+}
